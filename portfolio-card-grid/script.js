e2e6dcfa9a41991fedc1f9d537d66255b2984564
mattishenner <118067835+mattishenner@users.noreply.github.com>
2022-12-06 08:36:46 +0100

    Add files via upload

---
+++ portfolio-card-grid/script.js
@@ -0,0 +1,22 @@
+// ---------------------SCROLLING TRANSITIONS---------------------------
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hidden2Elements = document.querySelectorAll('.hidden2');
+hidden2Elements.forEach((el) => observer.observe(el));
+
+const hidden3Elements = document.querySelectorAll('.hidden3');
+hidden3Elements.forEach((el) => observer.observe(el));
+// ---------------------/SCROLLING TRANSITIONS/--------------------------
+
